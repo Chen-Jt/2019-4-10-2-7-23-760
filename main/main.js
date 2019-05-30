5b658d89016b65aa2ef31811bc00250df510f328
chenjt <1148013214@qq.com>
2019-05-30 17:54:04 +0800

    finished

--- main/main.js
+++ main/main.js
@@ -1,4 +1,11 @@
 module.exports = function main(inputs) {
-    // write your code here...
-    return "price";
+    cost = 6;
+    if (inputs.distance > 2) {
+        if (inputs.distance > 8)
+            cost += 4.8 + (inputs.distance - 8) * (1.2);
+        else
+            cost += 0.8 * (inputs.distance - 2);
+    }
+    cost += inputs.parkTime * 0.25;
+    return Math.round(cost);
 };
